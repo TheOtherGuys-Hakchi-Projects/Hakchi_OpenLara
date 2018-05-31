e4281c17d1ecc4dbcb78194968628858d03279b8
Ryan 'Swingflip' Hamlin <ryan@ryanhamlin.co.uk>
2018-05-31 17:37:22 +0100

    Fixed typos and error splash

--- CLV-Z-HAKCHI_OPENLARA/hakchi_OpenLara.sh
+++ CLV-Z-HAKCHI_OPENLARA/hakchi_OpenLara.sh
@@ -9,7 +9,7 @@ ok=0
 
 if [ -f "/usr/share/games/$GameName/$GameName.desktop" ]; then
 	OPENLARATrueDir=$(grep /usr/share/games/$GameName/$GameName.desktop -e 'Exec=' | awk '{print $2}' | sed 's/\([/\t]\+[^/\t]*\)\{1\}$//')
-	OPENLARAPortableFiles="$OPENLARATrueDir/OpenLara_Files"
+	OPENLARAPortableFiles="$OPENLARATrueDir/OpenLara_files"
 	ok=1
 fi
 
@@ -17,7 +17,7 @@ if [ "$ok" == 1 ]; then
    decodepng "$OPENLARATrueDir/Hakchi_OpenLara_assets/openlarasplash-min.png" > /dev/fb0;
    cd $OPENLARAPortableFiles
    chmod 755 $OPENLARAPortableFiles/OpenLara
-   exec ./OpenLara
+   exec $OPENLARAPortableFiles/OpenLara
 else
 	decodepng "$OPENLARATrueDir/Hakchi_OpenLara_assets/openlaraerror_files-min.png" > /dev/fb0;
 	sleep 5
