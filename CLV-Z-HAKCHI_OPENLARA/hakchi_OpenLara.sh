e4efef66ed9072c8d87580f8cd44caacee8b51ce
Ryan 'Swingflip' Hamlin <ryan@ryanhamlin.co.uk>
2018-05-31 16:57:37 +0100

    Initial Upload

---
+++ CLV-Z-HAKCHI_OPENLARA/hakchi_OpenLara.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+source /etc/preinit
+script_init
+
+WorkingDir=$(pwd)
+GameName=$(echo $WorkingDir | awk -F/ '{print $NF}')
+ok=0
+
+if [ -f "/usr/share/games/$GameName/$GameName.desktop" ]; then
+	OPENLARATrueDir=$(grep /usr/share/games/$GameName/$GameName.desktop -e 'Exec=' | awk '{print $2}' | sed 's/\([/\t]\+[^/\t]*\)\{1\}$//')
+	OPENLARAPortableFiles="$OPENLARATrueDir/OpenLara_Files"
+	ok=1
+fi
+
+if [ "$ok" == 1 ]; then
+   decodepng "$OPENLARATrueDir/Hakchi_OpenLara_assets/openlarasplash-min.png" > /dev/fb0;
+   cd $OPENLARAPortableFiles
+   chmod 755 $OPENLARAPortableFiles/OpenLara
+   exec ./OpenLara
+else
+	decodepng "$OPENLARATrueDir/Hakchi_OpenLara_assets/openlaraerror_files-min.png" > /dev/fb0;
+	sleep 5
+fi
