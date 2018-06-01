bee66bc36c3111b3f819a5391d8721afdbec8856
Ryan 'Swingflip' Hamlin <ryan@ryanhamlin.co.uk>
2018-06-01 12:55:06 +0100

    Added better file detection...

--- CLV-Z-HAKCHI_OPENLARA/hakchi_OpenLara.sh
+++ CLV-Z-HAKCHI_OPENLARA/hakchi_OpenLara.sh
@@ -6,6 +6,7 @@ script_init
 WorkingDir=$(pwd)
 GameName=$(echo $WorkingDir | awk -F/ '{print $NF}')
 ok=0
+filedetect=0
 
 if [ -f "/usr/share/games/$GameName/$GameName.desktop" ]; then
         OPENLARATrueDir=$(grep /usr/share/games/$GameName/$GameName.desktop -e 'Exec=' | awk '{print $2}' | sed 's/\([/\t]\+[^/\t]*\)\{1\}$//')
@@ -13,7 +14,20 @@ if [ -f "/usr/share/games/$GameName/$GameName.desktop" ]; then
         ok=1
 fi
 
-if [ "$ok" == 1 ]; then
+#check for gamefiles...
+[ -f "$OPENLARAPortableFiles/DATA/GYM.PHD"] && filedetect=1
+[ -f "$OPENLARAPortableFiles/GYM.PHD"] && filedetect=1
+[ -f "$OPENLARAPortableFiles/PSXDATA/GYM.PSX"] && filedetect=1
+#[ -f "$OPENLARAPortableFiles/DATA/GYM.SAT"] && filedetect=1 #SATURN (Not support yet)
+[ -f "$OPENLARAPortableFiles/data/ASSAULT.TR2"] && filedetect=1
+[ -f "$OPENLARAPortableFiles/assault.TR2"] && filedetect=1
+[ -f "$OPENLARAPortableFiles/DATA/ASSAULT.PSX"] && filedetect=1
+[ -f "$OPENLARAPortableFiles/data/JUNGLE.TR2"] && filedetect=1
+[ -f "$OPENLARAPortableFiles/DATA/JUNGLE.PSX"] && filedetect=1
+[ -f "$OPENLARAPortableFiles/level/1/GYM.PHD"] && filedetect=1
+[ -f "$OPENLARAPortableFiles/level/1/GYM.PSX"] && filedetect=1
+
+if [ "$ok" == 1 ] && [ "$filedetect" == 1 ]; then
    decodepng "$OPENLARATrueDir/Hakchi_OpenLara_assets/openlarasplash-min.png" > /dev/fb0;
    echo "1" > "/proc/sys/vm/overcommit_memory" #Unlock RAM limit. No reason not to...
    sleep 1 #Engine is so well built the splash needs to display for at least a second for looks...
